fcac27a97f445ed04bca5f43b76f763124867a70
Mike Stead <stead.mike@gmail.com>
2013-03-11 09:32:43 +1100

    update pathing to allow haxlib dev path to be set a little more easily

---
+++ src/org/hamcrest/AllClasses.hx
@@ -0,0 +1,66 @@
+/****
+* Copyright 2011 hamcrest.org. See LICENSE.txt
+****/
+
+package org.hamcrest;
+
+import org.hamcrest.AssertionException;
+import org.hamcrest.BaseDescription;
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.collection.IsArray;
+import org.hamcrest.collection.IsArrayContaining;
+import org.hamcrest.collection.IsArrayContainingInAnyOrder;
+import org.hamcrest.collection.IsArrayContainingInOrder;
+import org.hamcrest.collection.IsArrayWithSize;
+import org.hamcrest.collection.IsEmptyIterable;
+import org.hamcrest.collection.IsHashContaining;
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.hamcrest.collection.IsIterableContainingInOrder;
+import org.hamcrest.collection.IsIterableWithSize;
+import org.hamcrest.core.AllOf;
+import org.hamcrest.core.AnyOf;
+import org.hamcrest.core.CombinableMatcher;
+import org.hamcrest.core.DescribedAs;
+import org.hamcrest.core.Every;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsAnything;
+import org.hamcrest.core.IsCollectionContaining;
+import org.hamcrest.core.IsEqual;
+import org.hamcrest.core.IsInstanceOf;
+import org.hamcrest.core.IsNot;
+import org.hamcrest.core.IsNull;
+import org.hamcrest.core.IsSame;
+import org.hamcrest.core.ShortcutCombination;
+import org.hamcrest.core.StringContains;
+import org.hamcrest.core.StringEndsWith;
+import org.hamcrest.core.StringStartsWith;
+import org.hamcrest.core.SubstringMatcher;
+import org.hamcrest.CustomMatcher;
+import org.hamcrest.CustomTypeSafeMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.DiagnosingMatcher;
+import org.hamcrest.Exception;
+import org.hamcrest.FeatureMatcher;
+import org.hamcrest.internal.SelfDescribingValue;
+import org.hamcrest.internal.SelfDescribingValueIterator;
+import org.hamcrest.internal.VarArgMatchers;
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.MatchersBase;
+import org.hamcrest.number.IsCloseTo;
+import org.hamcrest.number.OrderingComparison;
+import org.hamcrest.SelfDescribing;
+import org.hamcrest.StringDescription;
+import org.hamcrest.TypedefChecker;
+import org.hamcrest.TypeSafeDiagnosingMatcher;
+import org.hamcrest.TypeSafeMatcher;
+
+@IgnoreCover
+class AllClasses
+{
+@IgnoreCover
+	public static function main():AllClasses {return new AllClasses();}
+@IgnoreCover
+	public function new(){trace('This is a generated main class');}
+}
+
