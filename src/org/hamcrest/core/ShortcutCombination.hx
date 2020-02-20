ccdb3923844571017de0214918eb39979d69dc24
kaikoga <info@mail.kaikoga.net>
2020-02-21 06:46:49 +0900

    `operator` is reserved keyword in Haxe 4.0.0-preview5 (#10)

--- src/org/hamcrest/core/ShortcutCombination.hx
+++ src/org/hamcrest/core/ShortcutCombination.hx
@@ -12,13 +12,13 @@ import org.hamcrest.Exception;
 class ShortcutCombination<T> extends BaseMatcher<T>
 {
     var matchers:Iterable<Matcher<T>>;
-    var operator:String;
+    var op:String;
 	
-    private function new(matchers:Iterable<Matcher<T>>, operator:String)
+    private function new(matchers:Iterable<Matcher<T>>, op:String)
     {
     	super();
         this.matchers = matchers;
-        this.operator = operator;
+        this.op = op;
     }
     
     function doesMatch(value:Dynamic, shortcut:Bool):Bool 
@@ -33,6 +33,6 @@ class ShortcutCombination<T> extends BaseMatcher<T>
     
     override public function describeTo(description:Description):Void
     {
-        description.appendList("(", " " + operator + " ", ")", matchers);
+        description.appendList("(", " " + op + " ", ")", matchers);
     }
 }
